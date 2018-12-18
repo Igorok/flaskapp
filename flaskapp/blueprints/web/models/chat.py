8ba53d800df90f492d389d483ff0fc7d2b0bbf1c
Igorok <igordubinin1@yandex.ru>
2018-12-18 23:22:06 +0300

    get private chat room

--- flaskapp/blueprints/web/models/chat.py
+++ flaskapp/blueprints/web/models/chat.py
@@ -3,15 +3,6 @@ from flaskapp.blueprints.web.models.user import UserModel
 from datetime import datetime, timedelta
 
 class ChatModel (Model):
-    '''
-    id: null,
-    userId: userId,
-    friendId: null,
-    date: null,
-    users: [],
-    messages: [],
-    '''
-
     """
     get private chat group by friendId
     :param token: - current token of user
@@ -22,23 +13,22 @@ class ChatModel (Model):
         if (not 'friendId' in kwargs):
             raise Exception('Group not found')
 
+        
         # check authentication and get data of current user
         uModel = UserModel()
         profileDict = uModel.getUserByToken(**kwargs)
 
-        selGrRows = ['id', 'user_id', 'friend_id', 'date'];
+        selGrRows = ['id', 'user_id', 'friend_id', 'date']
         selGrSql = '''select {0}, {1}, {2}, {3} from "chat_private"
             where ({1}=%s and {2}=%s) or ({1}=%s and {2}=%s)
-            '''.format(*selGrRows);
+            '''.format(*selGrRows)
 
         insGrSql = '''insert into "chat_private"
             ({1}, {2}, {3}) values (%s, %s, %s)
-            returning "{0}"'''.format(*selGrRows)
+            returning "{0}";'''.format(*selGrRows)
 
-        selGrByIdSql = '''select {0}, {1}, {2}, {3} from "chat_private"
-            where {0}=%s
-            '''.format(*selGrRows);
-
+        selGrByIdSql = 'select {0}, {1}, {2}, {3} from "chat_private" where {0}=%s;'.format(*selGrRows)
+        # try to get private group with friendId and current user id
         connection = self.connect_postgres()
         cursor = connection.cursor()
         cursor.execute(selGrSql, [
@@ -48,7 +38,7 @@ class ChatModel (Model):
             profileDict['id']
         ])
         selGrData = cursor.fetchone()
-
+        # if group not found, insert new group
         if (selGrData is None):
             cursor.execute(insGrSql, [
                 profileDict['id'],
@@ -63,6 +53,60 @@ class ChatModel (Model):
         ltd = self.list_to_dict(selGrRows)
         selGrDict = ltd(selGrData)
 
-        selGrDict['date'] = selGrDict['date'].strftime('%Y-%m-%d %H:%M:%S')
+        #get users from group
+        selUsrRows = ['id', 'login', 'dateReg', 'dateAct']
+        selUsrSql = '''select {0}, {1}, date_reg as {2}, date_act as {3} 
+            from "user" where {0}=any(%s);
+            '''.format(*selUsrRows)
+        cursor.execute(selUsrSql, [
+            [selGrDict['user_id'], selGrDict['friend_id']]
+        ])
+        selUsrData = cursor.fetchall()
+        ltdUsr = self.list_to_dict(selUsrRows)
+        usrList = []
+        uModel = UserModel()
 
-        return selGrDict
+        for u in selUsrData:
+            ud = ltdUsr(u)
+            onlData = uModel.checkOnline(ud)
+            usrList.append({
+                'id': ud['id'],
+                'login': ud['login'],
+                'online': onlData['online'],
+                'dateAct': onlData['dateAct'].strftime('%Y-%m-%d %H:%M:%S'),
+            })
+
+        #get messages from group
+        '''
+        "id" SERIAL PRIMARY KEY,
+        "user_id" SERIAL NOT NULL,
+        "chat_id" SERIAL NOT NULL,
+        "text" text,
+        "date" timestamp DEFAULT NULL
+        '''
+        selMsgRows = ['id', 'userId', 'chatId', 'text', 'date']
+        selMsgSql = '''select {0}, user_id as {1}, chat_id as {2}, {3}, {4}
+            from "chat_message" where chat_id=%s
+            order by id desc 
+            limit %s
+            ;'''.format(*selMsgRows)
+        cursor.execute(selMsgSql, [
+            selGrDict['id'],
+            100
+        ])
+        selMsgData = cursor.fetchall()
+        msgList = []
+        if (not selMsgData is None):
+            ltdMsg = self.list_to_dict(selMsgRows)
+            msgList = list(map(ltdMsg, selMsgData))
+
+        resDict = {
+            'id': selGrDict['id'],
+            'userId': selGrDict['user_id'],
+            'friendId': selGrDict['friend_id'],
+            'date': selGrDict['date'].strftime('%Y-%m-%d %H:%M:%S'),
+            'users': usrList,
+            'messages': msgList
+        }
+
+        return resDict
